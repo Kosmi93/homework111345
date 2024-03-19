c26f2ce5b5abb23e4421453d08dd6655ae84a2fc
Kosmi93 <k.evrei@mail.ru>
2024-03-19 15:52:04 +0300

    Удаление первичного добавления продуктов

--- src/main/java/bip/online/homework111345/repo/InMemoryProductRepo.java
+++ src/main/java/bip/online/homework111345/repo/InMemoryProductRepo.java
@@ -7,17 +7,11 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.LongStream;
 
 @Repository
 @Scope("session")
-public class InMemoryProductRepo implements IProductRepo{
+public class InMemoryProductRepo implements IProductRepo {
     private final List<Product> products = new ArrayList<>();
-
-    public InMemoryProductRepo() {
-        LongStream.range(0, 2)
-                .forEach(i->this.products.add(new Product(i,"Товар №%d".formatted(i),"Описание товара №%d".formatted(i))));
-    }
 
     @Override
     public List<Product> findAll() {
